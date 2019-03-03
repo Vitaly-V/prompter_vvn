7ba808f679b332f8013a203fb8bd376c00a0e0b3
Vitaly-V <gtopbox@gmail.com>
2019-03-03 17:47:00 +0200

    Extend Terminal class, create Options dummy data

--- example/main.dart
+++ example/main.dart
@@ -1,8 +1,14 @@
 import 'package:prompter_vvn/src/terminal.dart';
+import 'package:prompter_vvn/src/option.dart';
 
 void main() {
-  final t = Terminal();
-  t.clearScreen();
-  t.printPrompt('test dart');
-  t.collectInput();
+  final term = Terminal();
+  final options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f'),
+  ];
+
+  term.printOptions(options);
+  final response = term.collectInput();
+  print(response);
 }
